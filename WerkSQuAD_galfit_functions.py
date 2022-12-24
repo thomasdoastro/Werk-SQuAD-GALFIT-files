f40ed767a9cf303b7252521d025c3f55fa2e5b75
tmd37 <71458205+tmd37@users.noreply.github.com>
2022-12-24 00:03:36 -0800

    Update WerkSQuAD_galfit_functions.py
    Changed some variable names to be consistent throughout this file and accompanying notebook

--- WerkSQuAD_galfit_functions.py
+++ WerkSQuAD_galfit_functions.py
@@ -748,8 +748,7 @@ def find_inclination(best_fit_table):
     return best_fit_table
 
 #################### Function 12 ####################
-def apply_flags(best_fit_table, bad_fits_in, spiral_struct_in, overlap_obj_in, pixel_thresh,  edited_in, double_model_in):
-#def apply_flags(best_fit_table, bad_fits_in, internal_struct_in, pixel_thresh, dark_center_res_in, overlap_in):
+def apply_flags(best_fit_table, bad_fits_in, internal_struct_in, overlap_in, pixel_thresh,  edited_in, double_model_in):
     
     # Applies "is_good_fit" flag
     good_fit = [True] * len(best_fit_table)
@@ -780,12 +779,6 @@ def apply_flags(best_fit_table, bad_fits_in, spiral_struct_in, overlap_obj_in, p
             pass
         elif current_hlr < pixel_thresh:
             large_galaxy[i] = False
-            
-    # Applies "has_residual_dark_center" flag
-    # - REMOVING- it will be assumed that 'double_model' galaxies automatically have dark center in resid.
-    #has_dark_center_res = [False] * len(best_fit_table)
-    #for i in dark_center_res_in:
-    #    has_dark_center_res[i] = True
     
     ## Add for edited galaxies/ those varying from the initial parameters
     edited = [False] * len(best_fit_table)
@@ -804,6 +797,5 @@ def apply_flags(best_fit_table, bad_fits_in, spiral_struct_in, overlap_obj_in, p
     best_fit_table['is_big_galaxy'] = large_galaxy
     best_fit_table['edited'] = edited
     best_fit_table['double_model'] = double_model
-    # best_fit_table['has_dark_center_res'] = has_dark_center_res
     
     return best_fit_table
